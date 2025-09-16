0121847e6991a80bfd31b79ba6b1911440da4acd
QADRAX <carlos.delacuadra@devo.com>
2025-09-16 23:23:22 +0200

    chore: output 'standalone'

--- apps/moto125-ui/next.config.mjs
+++ apps/moto125-ui/next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'standalone',
   experimental: {
-    // habilita Server Actions si las vas a usar
     serverActions: { allowedOrigins: ["*"] }
   }
-  // Si luego cambias a next/image, descomenta y ajusta dominios:
-  // images: {
-  //   remotePatterns: [
-  //     { protocol: "http", hostname: "localhost", port: "1337", pathname: "/uploads/**" },
-  //     { protocol: "https", hostname: "tudominio-strapi.com", pathname: "/uploads/**" }
-  //   ]
-  // }
 };
 
 export default config;
