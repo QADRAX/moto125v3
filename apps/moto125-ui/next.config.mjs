35e0986bc8924c43e35df00e7adb087ba2c25ed9
QADRAX <carlos.delacuadra@devo.com>
2025-09-18 00:04:04 +0200

    chore: dockerized ui

--- apps/moto125-ui/next.config.mjs
+++ apps/moto125-ui/next.config.mjs
@@ -1,25 +1,8 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
-const monorepoRoot = path.join(__dirname, "../..");
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: "standalone",
   experimental: {
-    serverActions: { allowedOrigins: ["*"] },
-    /*
-    outputFileTracingRoot: monorepoRoot,
-    outputFileTracingIncludes: {
-      "*": [
-        "packages/data-mirror/**",
-        "packages/data-mirror-core/**",
-        "packages/data-mirror-worker/**",
-      ],
-    },*/
+    serverActions: { allowedOrigins: ["*"] }
   },
 };
 
